e9db78b387d227c717850cbe13b4a5e989289d53
Elshara Silverheart <sourceminded@gmail.com>
2019-12-17 00:57:17 -0800

    Added latest files.
    This is the latest Legacy commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PHPFox Legacy
  This is a legacy fork of v3.9. Modern PHPFox V4.8 is built using Unity Cloud. This version is completely self hosted offline. This code is retired and no longer being maintained by anyone, however v3 modules still exist. Requires php5.6 or older to run. 
+For more info, visit: http://store.phpfox.com/kb/article/135/installing-the-product
---
+++ theme/install/default/template/license.html.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * [PHPFOX_HEADER]
+ * 
+ * @copyright		[PHPFOX_COPYRIGHT]
+ * @author			Raymond Benc
+ * @package 		Phpfox
+ * @version 		$Id: license.html.php 3469 2011-11-07 16:51:48Z Raymond_Benc $
+ */
+ 
+defined('PHPFOX') or exit('NO DICE!'); 
+
+?>
+<iframe width="500" src="https://www.phpfox.com/terms/license/premium/simple/" name="iframe1" height="300" marginwidth="0" marginheight="0" frameborder="1" style="border:1px #B7B9BB solid; width:100%;" scrolling="yes">license text</iframe>
+<div class="table_clear mT10">
+	<form method="post" action="{url link=""$sUrl".license"}" id="install_form">
+		<div><input type="hidden" name="agree" value="1" /></div>
+		<input type="submit" name="submit" value="I Agree. Lets continue.." class="button" id="button" />
+	</form>
+</div>
